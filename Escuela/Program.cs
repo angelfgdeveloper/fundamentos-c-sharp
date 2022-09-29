640cea65fa74cf018bc3918d733d319be84c6080
Luis Angel <angelfgdeveloper@gmail.com>
2022-09-28 23:10:34 -0500

    Ciclos loop

--- Escuela/Program.cs
+++ Escuela/Program.cs
@@ -16,10 +16,29 @@ namespace CoreEscuela
             // Parametros opcionales y seleccion de argumento por pais:"bla bla"
             Escuela escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, pais: "Colombia", ciudad: "Bogota");
 
-            Curso curso1 = new Curso()
+            //Curso curso1 = new Curso()
+            //{
+            //    Nombre = "Curso de fundamentos C#",
+            //    // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
+            //};
+
+            //Curso curso2 = new Curso()
+            //{
+            //    Nombre = "Curso Intermedio C#",
+            //    // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
+            //};
+
+            //Curso curso3 = new Curso()
+            //{
+            //    Nombre = "Curso Avanzado C#",
+            //    // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
+            //};
+
+            Curso[] arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso()
             {
                 Nombre = "Curso de fundamentos C#",
-                // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
             };
 
             Curso curso2 = new Curso()
@@ -28,7 +47,9 @@ namespace CoreEscuela
                 // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
             };
 
-            Curso curso3 = new Curso()
+            arregloCursos[1] = curso2;
+
+            arregloCursos[2] = new Curso()
             {
                 Nombre = "Curso Avanzado C#",
                 // UniqueId = "c" => No lo podemos utilizar ya que lo encapsulamos
@@ -37,10 +58,67 @@ namespace CoreEscuela
             Console.WriteLine(escuela);
             Console.WriteLine("==================");
             // cw + tab => abre el log
-            Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
-            Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
-            Console.WriteLine(curso3);
+            //Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
+            //Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+            //Console.WriteLine(curso3);
+
+            // IMPRIMIR ARREGLOS
+            //Console.WriteLine(arregloCursos[0].Nombre);
+            //Console.WriteLine("Presione ENTER para continuar");
+            //Console.ReadLine(); // Espera a que presione enter
+            //Console.WriteLine(arregloCursos[5].Nombre); // Truena porque no existe ese elemento
+
+            ImprimirCursosWhile(arregloCursos);
+
+            Console.WriteLine("==================");
+            ImprimirCursosDoWhile(arregloCursos);
+
+            Console.WriteLine("==================");
+            ImprimirCursosFor(arregloCursos);
+
+            Console.WriteLine("==================");
+            ImprimirCursosForEach(arregloCursos);
+
         }
 
+        private static void ImprimirCursosWhile(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[contador].Nombre}, ID: {arregloCursos[contador].UniqueId}");
+                // contador = contador + 1;
+                // contador += 1;
+                contador++;
+            }
+        }
+
+        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
+        {
+            // Entra la primera vez
+            int contador = 0;
+            do
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[contador].Nombre}, ID: {arregloCursos[contador].UniqueId}");
+                contador++;
+            } while (contador < arregloCursos.Length);
+        }
+
+        private static void ImprimirCursosFor(Curso[] arregloCursos)
+        {
+            for (int i = 0; i < arregloCursos.Length; i++)
+            {
+                Console.WriteLine($"Nombre: {arregloCursos[i].Nombre}, ID: {arregloCursos[i].UniqueId}");
+
+            }
+        }
+
+        private static void ImprimirCursosForEach(Curso[] arregloCursos)
+        {
+            foreach (Curso curso in arregloCursos)
+            {
+                Console.WriteLine($"Nombre: {curso.Nombre}, ID: {curso.UniqueId}");
+            }
+        }
     }
 }
